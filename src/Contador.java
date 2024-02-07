88eb538bf0c42ab550e3ba658ff293b0bee20ace
Ycaro <Ycaro.araujo1@outlook.com>
2024-02-06 21:23:52 -0300

    Commit Inicial

---
+++ src/Contador.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Contador {
+    public static void main(String[] args) {
+
+        try {
+            Scanner terminal = new Scanner(System.in);
+            System.out.println("Digite o primeiro parâmetro");
+            int parametroUm = terminal.nextInt();
+            System.out.println("Digite o segundo parâmetro");
+            int parametroDois = terminal.nextInt();
+
+            contar(parametroUm, parametroDois);
+
+        } catch (ParametrosInvalidosException exception) {
+            System.out.println("O Segundo parametro deve ser maior que o primeiro");
+
+        }
+
+    }
+
+    static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
+        if (parametroUm > parametroDois)
+            throw new ParametrosInvalidosException();
+
+        int contagem = parametroDois - parametroUm;
+        // realizar o for para imprimir os números com base na variável contagem
+        for (int x = 0; x < contagem; x++) {
+            System.out.println("Imprimindo o número " + (x + 1));
+        }
+    }
+}
